1a951e32321115c8455a9371d1b2f2dd22b22c2a
Lynn Mwaniki <Lynn.mwaniki@student.moringaschool.com>
2022-08-23 10:59:14 +0300

    not so sure

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  
+  //To create the form function, we first grab the form element <for id="create-task-form"
+  const newTaskForm = document.getElementById("create-task-form");
+  
+  //access the list function to be able to add a newTask to it 
+  const taskList = document.getElementById("tasks")
+
+  //add EventListener to get accesses to targets, and the new entered task
+  //update TaskList with newTask and update the DOM (show task on screen) to do this create a pTag, assign the value and append it
+  newTaskForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const newTaskInput = e.target.querySelector("#new-task-description").value
+    const taskPTag = document.createElement('p')
+    taskPTag.textContent = newTaskInput 
+    taskList.appendChild(taskPTag)
+
+
+    console.log(taskList)
+    console.log(newTaskInput)
+
+  })
+  
+}); 
