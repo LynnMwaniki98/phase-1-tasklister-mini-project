cf82a17c5f09dc7f20e2a44b72b04546d433fd37
Lynn Mwaniki <Lynn.mwaniki@student.moringaschool.com>
2022-08-24 12:07:29 +0300

    just a trial

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   
-  //To create the form function, we first grab the form element <for id="create-task-form"
   const newTaskForm = document.getElementById("create-task-form");
   
-  //access the list function to be able to add a newTask to it 
   const taskList = document.getElementById("tasks")
 
-  //add EventListener to get accesses to targets, and the new entered task
-  //update TaskList with newTask and update the DOM (show task on screen) to do this create a pTag, assign the value and append it
   newTaskForm.addEventListener("submit", (e) => {
     e.preventDefault()
 
@@ -15,11 +11,51 @@ document.addEventListener("DOMContentLoaded", () => {
     const taskPTag = document.createElement('p')
     taskPTag.textContent = newTaskInput 
     taskList.appendChild(taskPTag)
-
+    const btn = document.createElement('button')
+      btn.addEventListener('click', handleDelete)
+        btn.textContent = 'x'
 
     console.log(taskList)
     console.log(newTaskInput)
-
   })
+  function handleDelete(e){
+    e.target.parentNode.remove()
+  } 
   
-}); 
+});
+function sortTaskList(e){
+  e.target.parentNode.sort()
+
+}
+
+
+
+/*document.addEventListener("DOMContentLoaded", () => {
+const form = document.getElementById('create-task-form');
+form.addEventListener('submit', (e) =>{
+  e.preventDefault()
+  taskList(e.target.new-task-description.value)
+  form.reset()
+});
+
+});
+
+function taskList(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  //let btn1 = document.createElement('select')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+p.textContent = `${todo}`
+p.appendChild(btn)
+console.log(p)
+document.querySelector('#tasks').appendChild(p)
+
+}
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+function sortToDoList(e){
+  e.target.parentNode.sort()
+
+}*/
